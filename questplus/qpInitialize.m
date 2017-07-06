b23218c72f76ae99f1f4aa1e5bf41fb8e91e4196
David Brainard <brainard@psych.upenn.edu>
2017-07-06 13:41:23 -0400

    combec -> allcomb
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

--- questplus/qpInitialize.m
+++ questplus/qpInitialize.m
@@ -36,21 +36,22 @@ function questData = qpInitialize(varargin)
 questData = qpParams(varargin{:});
 
 %% Convert list specifying stimlus parameters domain ...
-% to a matrix, where each row of the matrix
-% is the parameters for one of the possible stimuli in
-% the domain.
+% to a matrix, where each row of the matrix is the parameters for one of
+% the possible stimuli in the domain.
 %
-% The idea of using combdavec to convert the domain list to this particular
+% The idea of using combvec to convert the domain list to this particular
 % matrix format, here and just below, originated with a separate and
 % earlier (2016) Matlab implementation% of QUEST+ written by P R Jones
-% <petejonze@gmail.com>.
-questData.stimParamsDomain = combvec(questData.stimParamsDomainList{:})';
+% <petejonze@gmail.com>.  I subsequently switched to the allcomb function
+% from Matlab Central, to avoid making people own the nnet toolbox just to
+% have combvec.
+questData.stimParamsDomain = allcomb(questData.stimParamsDomainList{:});
 [questData.nStimParamsDomain,questData.nStimParams] = size(questData.stimParamsDomain);
 
 %% Convert psi params domain list to a matrix, where
 % each row of the matrix is the parameters for one of
 % the possibilities in the domain.
-questData.psiParamsDomain = combvec(questData.psiParamsDomainList{:})';
+questData.psiParamsDomain = allcomb(questData.psiParamsDomainList{:});
 [questData.nPsiParamsDomain,questData.nPsiParams] = size(questData.psiParamsDomain);
 
 %% Initilize logLikeihood and posterior
@@ -70,59 +71,7 @@ for jj = 1:questData.nPsiParamsDomain
         questData.qpPF(questData.stimParamsDomain,questData.psiParamsDomain(jj,:));
 end
 
-
 %% Initialize table of expected entropies
 questData.expectedNextEntropiesByStim  = qpUpdateExpectedNextEnropiesByStim(questData);
 
 end
-
-% {QpOutcome -> (SimulatedObserver[#1, 
-%      QpPFWeibull, {-20, 3.5, 0.5, 0.02}] &), 
-%  QpPNames -> {"Threshold (dB)", "Slope", "Guess", "Lapse"}, 
-%  QpXNames -> {"Contrast (dB)"}, 
-%  QpXSamples -> {{-40, -39, -38, -37, -36, -35, -34, -33, -32, -31, \
-% -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, \
-% -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1,
-%      0}}, QpPSamples -> {{-40, -39, -38, -37, -36, -35, -34, -33, \
-% -32, -31, -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, \
-% -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, \
-% -3, -2, -1, 0}, {3.5}, {0.5}, {0.02}}, QpPsi -> QpPFWeibull, 
-%  QpPrior -> "Constant", Verbose -> False}
-
-
-% QpInitialize::usage = 
-%   "QpInitialize[options___Rule]\nInitialize the data structure for a \
-% QUEST+ experiment. The options determine the configuration of the \
-% experiment. The function returns a large data structure consisting of \
-% the elements: {data, posterior, posteriors, nextindex, xnext, depth, \
-% likelihood, {options}}. When there is more than one condition, a \
-% structure can be created for each.";
-% 
-% QpInitialize[options___Rule] := Block[
-%   {pnames, xsamples, psamples, psi, outcome, prior, response, data, 
-%    depth, nxd, npd, nld, likelihood, likexprior, probability, 
-%    posteriors, entropies, nextindex, xnext, posterior, verbose},
-%   
-%   {xsamples, psamples, psi, response, prior, 
-%     verbose} = {QpXSamples, QpPSamples, QpPsi, QpOutcome, QpPrior, 
-%       Verbose} /. {options} /. Options[QuestPlus];
-%   
-%   data = {};
-%   nxd = Length[xsamples];
-%   npd = Length[psamples];
-%   nld = nxd + npd + 1;
-%   depth = nxd + 1;
-%   If[prior === "Constant",
-%    prior = UniformArray[Length /@ psamples]];
-%   prior = Developer`ToPackedArray[prior];
-%   likelihood = 
-%    Chop[N[Outer[psi[Take[{##}, nxd], Drop[{##}, nxd]] &, 
-%       Sequence @@ Join[xsamples, psamples]]]];
-%   likelihood = Transpose[likelihood, RotateLeft[Range[nld]]];
-%   likelihood = Developer`ToPackedArray[likelihood];
-%   If[verbose, Print["samples = ", Times @@ Dimensions[likelihood]]];
-%   posterior = prior;
-%   xnext = nextindex = posteriors = 0;
-%   {data, posterior, posteriors, nextindex, xnext, depth, 
-%    likelihood, {options}}
-%   ]
