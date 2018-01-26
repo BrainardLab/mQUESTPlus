d610f355f89793284a4e13a21e12f0433ee5ac62
David Brainard <brainard@psych.upenn.edu>
2018-01-26 08:24:54 -0500

    Improving help text as per DGP suggestions

--- Contents.m
+++ Contents.m
@@ -2,18 +2,19 @@
 %
 % MATLAB implementation of Watson's QUEST+.
 % 
-% The method and Mathematica code are described in the paper:
-% Watson, A. B. (2017).  "QUEST+: A general multidimensional Bayesian adaptive psychometric method".
-% Journal of Vision, 17(3):10, 1-27,
+% The method and Mathematica code are described in the paper: Watson, A. B.
+% (2017).  "QUEST+: A general multidimensional Bayesian adaptive
+% psychometric method". Journal of Vision, 17(3):10, 1-27,
 % http://jov.arvojournals.org/article.aspx?articleid=2611972.
 %  
-% See README.md for more info. A good way to get started with mQUESTPlus
-% is to look over the demos. See "help mQUESTPlus/demos".  You may also
-% want to read the help text of qpRun ("help qpRun") early on.
+% See README.md in the mQUESTPlus repository for more info. A good way to
+% get started with mQUESTPlus is to look over the demos. See "help
+% mQUESTPlus/demos".
 %
 % dataproc                      - Functions for data manipulation
 % demos                         - Demonstration/test programs.
-% mathworkscentral              - Dependencies obtained from mathworks central and other internet sources.
+% mathworkscentral              - Dependencies obtained from mathworks
+%                                 central and other internet sources.
 % printplot                     - Support for printing and plotting.
 % psifunctions                  - Psychometric functions.
 % questplus                     - The core QUEST+ routines.
--- demos/Contents.m
+++ demos/Contents.m
@@ -1,14 +1,26 @@
 % mQUESTPlus - demos
 %
-% Demonstration/test programs
+% Demonstration/test programs.  Two key demos are
+%   1) qpQuestPlusPaperSimpleExampleDemo.  This runs several of the basic
+%   demonstrations from the Watson (2017) QUEST+ paper, showing usage for
+%   function qpRun.
+%
+%   2) qpQuestPlusCoreFunctionDemo. This illustrates how you can call the
+%   core functions of mQUESTPlus directly, rather than letting qpRun
+%   orchestrate your experiment.
 %
 % qpDataProcDemo                - Demonstrate/test the data processsing routines.
 % qpPsiFunctionDemo             - Demonstrate/test the psychometric function routines.
-% qpQuestPlusCoreFunctionDemo   - Show basic use of QUEST+ core functions, called directly.
-%                                 This is the one to look at if you don't want to hand off
-%                                 control of your experiment to qpRun.
-% qpQuestPlusCSFDemo            - Demonstrate/test QUEST+ at work on parametric CSF estimation.
-% qpQuestPlusCircularCatDemo    - Demonstrate/test QUEST+ at work on categorization of a circular variable.
-% qpQuestPlusPaperSimpleExamplesDemo - Demonstrate/test QUEST+ on some simple example problems.
+% qpQuestPlusCoreFunctionDemo   - Show basic use of QUEST+ core functions,
+%                                 called directly. This is the one to look
+%                                 at if you don't want to hand off control
+%                                 of your experiment to qpRun.
+% qpQuestPlusCSFDemo            - Demonstrate/test QUEST+ at work on parametric
+%                                 CSF estimation.
+% qpQuestPlusCircularCatDemo    - Demonstrate/test QUEST+ at work on
+%                                 categorization of a circular variable.
+% qpQuestPlusPaperSimpleExamplesDemo - Demonstrate/test QUEST+ on some
+%                                 simple example problems.  Illustrates
+%                                 basic usage of function qpRun.
 % qpRunAllDemos                 - Run through all of the QUEST+ demos.
 % qpUtilityDemo                 - Demonstrate/test the qp utility routines.
