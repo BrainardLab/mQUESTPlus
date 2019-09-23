703cec7703ada61e63dae5892f71cc2f47b60d91
David Brainard <brainard@psych.upenn.edu>
2019-09-23 16:55:53 -0700

    Add function to computer posterior mean.

--- utilities/Contents.m
+++ utilities/Contents.m
@@ -11,6 +11,7 @@
 % qpListMinArg                  - Return the index of the smallest element in a vector/matrix.
 % qpLogLikelihood               - Compute log likelihood of a stimulus count data array.
 % qpNLogP                       - Compute n*log(p) and handle cases where one or both args are zero.
+% qpPosteriorMean               - Find the posterior mean
 % qpStimIndexToStim             - Find stimulus in stimDomain corresonding to stimIndex.
 % qpStimToStimIndex             - Find index of passed stimulus in the stimulus domain.
 % qpUniformArray                - Create an array of passed size whose values sum to 1.
---
+++ utilities/qpPosteriorMean.m
@@ -0,0 +1,50 @@
+function posteriorMean = qpPosteriorMean(posterior,psiParamsDomainList,varargin)
+%qpPosteriorMean  Compute the posterior mean of the PF parameters
+%
+% Usage:
+%     posteriorMean = qpPosteriorMean(posterior,psiParamsDomainList)
+%
+% Description:
+%     Compute the posterior mean
+%
+% Input:
+%     posterior      Column vector giving posterior over parameters
+%                    This should sum to 1.
+%     psiParamsDomainList  Matrix. Each row gives a vector of PF
+%                    parameters.  The number of rows should match
+%                    the number of entries in posterior.
+%
+% Output:
+%     posteriorMean  The mean of the posterior
+%
+% Optional key/value pairs
+%     None
+
+% 09/23/19 dhb  Wrote it.
+
+%% Parse input
+%
+% This routine gets called many many times and should be as fast as
+% possible.  The input parser is slow.  So we forego arg checking and
+% optional key/value pairs.  The code below shows how they would look.
+%
+% p = inputParser;
+% p.addRequired('posterior',@isnumeric);
+% p.addRequired('psiParamsDomainList',@isnumeric);
+% p.parse(probArray,varargin{:});
+
+%% Check that probabilities sum to something close to 1
+tolerance = 1e-7;
+assert(abs(sum(posterior(:))-1) < tolerance);
+
+%% Dimension checks
+if (~iscolumn(posterior))
+    error('Passed posterior must be a column vector');
+end
+if (length(posterior) ~= size(psiParamsDomainList,1))
+    error('Posterior and psiParamsDomainList are not matched in dimensions');
+end
+
+%% Compute the posterior mean
+posteriorMean = sum(bsxfun(@times,psiParamsDomainList,posterior),1);
+
